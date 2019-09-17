408509b5f97e38fefaadc665c1d836c210c1f28a
Bulent Vardal <bbvardal@gmail.com>
2019-09-17 15:25:47 +0300

    New Golang web server Dockerfile

---
+++ golangWebServer/main.go
@@ -0,0 +1,21 @@
+package main
+
+import ("fmt"
+		"net/http")
+func index_handler(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w,"Benim")
+}
+func about_handler(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w,"Hakkımda")
+}
+
+func main(){
+
+	http.HandleFunc("/",index_handler)
+	http.HandleFunc("/about",about_handler)
+	http.ListenAndServe(":8000",nil)
+
+
+
+
+}
